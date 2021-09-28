d932e56c2ec923c18dead620702264bc4a931a12
airmanxtw <airmanx@stust.edu.tw>
2021-09-28 23:39:07 +0800

    first vue3

---
+++ src/test.js
@@ -0,0 +1,11 @@
+import {createApp} from "vue"
+const Init={
+    data() {
+        return {
+            title:"hello"
+        }
+    },
+}
+
+createApp(Init).mount("#app");
+
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const webpack = require('webpack');
+//const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+const RemovePlugin = require('remove-files-webpack-plugin');
+
+const filename = "test.0.0.1.js";
+//const fpath="C:/Users/airmanx/source/repos/WS/WS/Javascript";
+const fpath = path.resolve(__dirname, 'dist');
+
+
+module.exports = {
+    entry: './src/test.js',
+    cache:true,
+    output: {
+        filename,
+        path: fpath,
+        //path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    //mode:'production',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        // Requires sass-loader@^7.0.0
+                        options: {
+                            implementation: require('sass'),
+                            indentedSyntax: true // optional
+                        },
+                        // Requires sass-loader@^8.0.0
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                indentedSyntax: true // optional
+                            },
+                        },
+                    },
+                ],
+            },
+
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin(),
+        new RemovePlugin({
+            before: {
+                allowRootAndOutside: true,
+                // parameters for "before normal compilation" stage.
+                include: [
+                    `${fpath}/${filename}`
+                ]
+            },
+            watch: {
+                // parameters for "before watch compilation" stage.
+            },
+            after: {
+                // parameters for "after normal and watch compilation" stage.
+            }
+        }),
+        new webpack.IgnorePlugin({
+            resourceRegExp: /^\.\/locale$/,
+            contextRegExp: /moment$/,
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        }
+    }
+};
